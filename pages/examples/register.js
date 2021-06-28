6b54ebc8dcc4a521337a8bef58e80b21f294d4ce
init-self <54549746+init-self@users.noreply.github.com>
2021-06-28 21:28:28 +0530

    Updated Error Messages display and added comments

--- pages/examples/register.js
+++ pages/examples/register.js
@@ -15,36 +15,59 @@ $(function()
         
         if(retype === formData.password)
         {
+            // are the field's values according to specified length?
             if((formData.name.length || formData.email.length) != 0 && formData.password.length > 6)
             {
+                // is the checkbox checked?
                 if(formData.check)
                 {
+                    // send the data to database and return error messages if any
                     $.post("../database/register.php", formData, (data,staus,xhr) =>
                     {
                         var data = JSON.parse(data);
                         // console.log(data);
                         if(data.success)
                         {
-                            window.location = "../../index.html";
+                            // show successful signup message
+                            $('#ErrorMessage .alert').css('display', 'block');
+                            $('#ErrorMessage div[class="alert"]').attr('class', 'alert alert-success');
+                            $('#ErrorMessage i').attr('class', 'text-success').html("Successfully Registered! Proceeding to <b>Login Page...</b>");   
+                            window.setTimeout(function()
+                            {
+                                window.location = "../../index.html";
+                            }, 1500)
 
                         }else
                         {
                             // show message box if validation is false
                             $('#ErrorMessage .alert').css('display', 'block');
                             $('#ErrorMessage .text-danger').text(data.errors);
+                            window.setTimeout(function()
+                            {
+                                $('#ErrorMessage .alert').css('display', 'none');
+                            }, 1000)
                         }
                     })
                 }else
                 {
+                    // if data is not according to specified length.
                     $('#ErrorMessage .alert').css('display', 'block');
                     $('#ErrorMessage .text-danger').text("Please read and accept our Terms. ");
-                }
+                    window.setTimeout(function()
+                    {
+                        $('#ErrorMessage .alert').css('display', 'none');
+                    }, 1000)
+        }
                 
             }else
             {
                 // show message box if fields are empty or password does not meet criteria
                 $('#ErrorMessage .alert').css('display', 'block');
                 $('#ErrorMessage .text-danger').text("Invalid Details!. Please fill all the fields according to the specified criteria. ");
+                window.setTimeout(function()
+                {
+                    $('#ErrorMessage .alert').css('display', 'none');
+                }, 1000)
             }
                 
         }else
