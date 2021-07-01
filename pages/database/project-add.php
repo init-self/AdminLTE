434b7db5fa0a2dd390896e07af20f744bdec4a54
init-self <54549746+init-self@users.noreply.github.com>
2021-07-01 21:45:25 +0530

    updated comments and error messages

--- pages/database/project-add.php
+++ pages/database/project-add.php
@@ -17,9 +17,10 @@ $data = array();
 
 
 
-
+// checck the request flag from project add page
 if(isset($_REQUEST['flag']) && $_REQUEST['flag'] === "insert")
 {
+    // extract the variables
     $validate = $_REQUEST['validate'];
     $name = $_REQUEST['name'];
     $desc = $_REQUEST['desc'];
@@ -30,8 +31,10 @@ if(isset($_REQUEST['flag']) && $_REQUEST['flag'] === "insert")
     $totalSpent = $_REQUEST['spent'];
     $duration = $_REQUEST['duration'];
 
+    // is insertion request made ?
     if(isset($_REQUEST['flag']) && $_REQUEST['flag'] === 'insert')
     {
+        // make the queries to insert data into projects  and budgets
         $query = "INSERT INTO projects (Name, Description, Status, ClientCompany, ProjectLeader) VALUES(:name, :desc, :status, :client, :leader);";
         $query_budget = "INSERT INTO budgets (Budget, Spent, Duration) VALUES (:estimatedBudget, :totalSpent, :duration);";
 
@@ -39,6 +42,7 @@ if(isset($_REQUEST['flag']) && $_REQUEST['flag'] === "insert")
         $stmt_budget = $conn -> prepare($query_budget);
         if($stmt && $stmt_budget)
         {
+            // update the values
             $stmt -> bindParam(":name", $name);
             $stmt -> bindParam(":desc", $desc);
             $stmt -> bindParam(":status", $status);
@@ -50,20 +54,21 @@ if(isset($_REQUEST['flag']) && $_REQUEST['flag'] === "insert")
 
             if($stmt -> execute() && $stmt_budget -> execute())
             {
-                $data['success'] = true;
+                $data['success'] = true; // success
             }else
             {
-                $data['success'] = false;
+                $data['success'] = false; // failure
                 $data['message'] = "Could not save records. Please try again. ";
             }
         }
     }else
     {
-        // respond message
+        // respond message for incorrect flag request
         $data['message'] = "Insertion Error! Please try again";
     }
 }
 
+// return data
 echo json_encode($data);
 
 
