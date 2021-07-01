cfbb1514f3059624595740158cf12b5f927113d6
init-self <54549746+init-self@users.noreply.github.com>
2021-07-01 22:09:09 +0530

    updated comments

--- pages/examples/project-add.js
+++ pages/examples/project-add.js
@@ -2,7 +2,7 @@
  * Editing done from line 864
  */
 
-
+// emptiness validation
 function validate(arr)
 {
     for(let elem of arr)
@@ -15,10 +15,13 @@ function validate(arr)
     return true;
 }
 
+
 $(document).ready(function()
 {
+    // on clicking the submit button
     $('#submit').on('click', () =>
     {
+        // extract the variables 
         var formData = {
             flag: "insert",
             name: $('#inputName').val(),
@@ -34,21 +37,24 @@ $(document).ready(function()
         if (validate(Object.values(formData)))
         {
             formData['validate'] = true;
+
             $.post("../database/project-add.php", formData, (data, status, xhr) =>
             {
                 var data = JSON.parse(data);
                 // console.log(data) // debugging purposes
                 if(data.success)
                 {
+                    // modal on successfull addition of project
                     $('#Modal').modal('show')
                     $('#head').attr('class', 'text-success').html("Success<i style=\"font-size: 2rem;\" class=\"bi bi-check2-circle mx-2\"</i>")
                     $('#details').attr('class', 'text-success' ).text("Records saved Successfully!");
                     setTimeout(function()
                     {
-                        window.location = "./projects.php";
+                        window.location = "./projects.php"; // redirect after 1 sec of showing modal
                     }, 1000)
                 }else
                 {
+                    // Error Modal
                     $('#Modal').modal('show')   
                     $('#head').html("<i class=\"bi bi-exclamation-lg mx-2\"</i>Error")
                     $('#details').text(data.message);
@@ -56,6 +62,7 @@ $(document).ready(function()
             })
         }else
         {
+            // Error modal on emptiness
             $('#Modal').modal('show');
             $('#head').attr('class', 'text-danger').html("<i class=\"bi bi-exclamation-lg mx-2\"></i>Error");
             $('#details').text("Please fill all the details carefully. ");
